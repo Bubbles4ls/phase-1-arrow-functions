16c3a8ae3504f897a49dbda06fbb507397eadd8a
Surayah Holt <+>
2023-03-20 02:16:16 -0400

    lab complete

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (num1, num2) => num1 / num2;
+   let num1 = 2000
+   let num2 = 100
+
+divide();
+
+const square = (x) => {
+       
+    x=2
+
+    return x*x
+     
+}
+
+const add = ( a , b) => a + b ; 
